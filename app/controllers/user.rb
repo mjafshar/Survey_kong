cc1abd0cc9cac55fe305a6a4209f511eb894852c
Apprentice <apprentice@dbc-chicago-010.local>
2014-04-11 15:38:35 -0500

    Added user authentication and pages.

---
+++ app/controllers/user.rb
@@ -0,0 +1,40 @@
+get '/users/login' do
+  erb :'users/login'
+end
+
+get '/users/index' do
+  erb :'users/index'
+end
+
+post '/users/login' do
+  @user = User.where(email: params[:email]).first
+  if @user && @user.authenticate(params[:password])
+    session[:value] = @user.id
+    redirect to ('/users/index')
+  else
+    redirect to ('/users/login')
+  end
+end
+
+get '/users/logout' do
+  session.clear
+  redirect to ('/')
+end
+
+get '/users/signup' do
+  erb :'users/signup'
+end
+
+post '/users/signup' do
+  user_info = params[:user_info]
+  @user = User.new(user_info)
+
+  if @user.save
+    session[:value] = @user.id
+    redirect to ('/users/index')
+  else
+    redirect to ('/users/login')
+  end
+end
+
+
