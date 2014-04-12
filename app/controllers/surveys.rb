f064319aef55a27110ecb4f5ab468be0dc2f0716
Apprentice <apprentice@dbc-chicago-009.local>
2014-04-12 17:34:13 -0500

    Added ability to create multiple questions and choices for a survey.

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -12,29 +12,39 @@ get "/survey/:id" do
 end
 
 post '/survey/new' do
-  @user = User.find(session[:value])
+  user = User.find(session[:value])
   if params[:privacy] == "true"
     @private = true
   else
     @private = false
   end
-  @survey = Survey.new(
+  questions = params[:questions]
+  puts "================================================"
+  puts "This is the survey title: #{params[:title]}"
+  puts "These are questions: #{questions}"
+  puts "This is question 1: #{questions[:question1][:question]}"
+  puts "This is question 2: #{questions[:question1][:answers]}"
+  puts "Is this survey private? #{params[:privacy]}"
+  puts "================================================"
+  survey = Survey.new(
     title: params[:title],
-    user_id: @user.id,
+    user_id: user.id,
     :private => @private
     )
 
-  if @survey.save
-    @question = Question.create(
-      description: params[:question],
-      survey_id: @survey.id
-      )
-
-    @choice = Choice.create(
-      possible_choice: params[:chosen_answer],
-      question_id: @question.id
-      )
-
+  if survey.save
+    questions.each do |new_question|
+      question = Question.create(
+        description: new_question.last[:question],
+        survey_id: survey.id
+        )
+      new_question.last[:answers].each_value do |choice|
+        Choice.create(
+          possible_choice: choice,
+          question_id: question.id
+        )
+      end
+    end
     redirect to ('/users/index')
   else
     erb :"survey/new"
